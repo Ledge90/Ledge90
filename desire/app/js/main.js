9b899259d56ea16efdc143a87326ad1448cf3dfc
Misha Shevch <jetblack22890@gmail.com>
2022-05-28 09:01:47 +0300

    site

---
+++ desire/app/js/main.js
@@ -0,0 +1,28 @@
+// Выезжающая панель справа
+
+$(function () {
+
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open');
+    });
+
+    if($(window).width() < 651){
+        $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+    }
+
+    var mixer = mixitup('.gallery__inner' , {
+        load: {
+            filter: '.living'
+        }
+    });
+
+
+})
+
